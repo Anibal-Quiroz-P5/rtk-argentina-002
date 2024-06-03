d20f2cfdcf490defa84e8334b72fd6465e09566b
Anibal-Quiroz-P5 <anibal.quiroz.p5@gmail.com>
2024-06-03 13:33:49 -0300

    Configure TypeScript and Next.js to ignore build errors2

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 // next.config.js
-/* module.exports = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-}; */
-
-
 module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
   },
 };
